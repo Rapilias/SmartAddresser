25d3c7d42894747af159f0eab71d19b46d593af3
Rapilias <rapilias.rest@gmail.com>
2023-12-22 04:39:19 +0900

    Optimize move entries

--- Assets/SmartAddresser/Editor/Core/Models/Services/ApplyLayoutRuleService.cs
+++ Assets/SmartAddresser/Editor/Core/Models/Services/ApplyLayoutRuleService.cs
@@ -1,9 +1,9 @@
+using System.Collections.Generic;
 using System.Linq;
 using SmartAddresser.Editor.Core.Models.LayoutRules;
 using SmartAddresser.Editor.Foundation.AddressableAdapter;
 using SmartAddresser.Editor.Foundation.AssetDatabaseAdapter;
 using SmartAddresser.Editor.Foundation.SemanticVersioning;
-using UnityEngine;
 
 namespace SmartAddresser.Editor.Core.Models.Services
 {
@@ -55,17 +55,13 @@ namespace SmartAddresser.Editor.Core.Models.Services
 
             // Add all entries to the addressable asset system.
             var versionExpression = _layoutRule.Settings.VersionExpression;
-            foreach (var assetPath in _assetDatabaseAdapter.GetAllAssetPaths())
-            {
-                var guid = _assetDatabaseAdapter.AssetPathToGUID(assetPath);
-                TryAddEntry(guid, false, versionExpression.Value);
-            }
+            TryAddEntries(_assetDatabaseAdapter.GetAllAssetPaths(), false, versionExpression.Value);
         }
 
         /// <summary>
         ///     Apply the layout rule to the addressable settings.
         /// </summary>
-        /// <param name="assetGuid"></param>
+        /// <param name="pathes"></param>
         /// <param name="doSetup">
         ///     If true, setup rules before providing.
         ///     When you call this method multiple times, set this false and call <see cref="Setup" /> before.
@@ -76,9 +72,79 @@ namespace SmartAddresser.Editor.Core.Models.Services
         ///     If the layout rule was applied to the addressable asset system, return true.
         ///     Returns false if no suitable layout rule was found.
         /// </returns>
-        public bool TryAddEntry(string assetGuid, bool doSetup, string versionExpression = null)
+        public void TryAddEntries(string[] pathes, bool doSetup, string versionExpression = null)
         {
-            var assetPath = _assetDatabaseAdapter.GUIDToAssetPath(assetGuid);
+            var groupedGuids = new Dictionary<string, List<string>>();
+            foreach (var assetPath in pathes)
+            {
+                var assetGuid = _assetDatabaseAdapter.AssetPathToGUID(assetPath);
+                var assetType = _assetDatabaseAdapter.GetMainAssetTypeAtPath(assetPath);
+                var isFolder = _assetDatabaseAdapter.IsValidFolder(assetPath);
+
+                // If the layout rule was not found, return false.
+                if (!_layoutRule.TryProvideAddressAndAddressableGroup(assetPath, assetType, isFolder, doSetup,
+                        out _, out var addressableGroup
+                    ))
+                    continue;
+
+                // If the layout rule is found but the addressable asset group has already been destroyed, return false.
+                if (addressableGroup == null)
+                    continue;
+
+                // Check the version if it is specified.
+                if (!string.IsNullOrEmpty(versionExpression))
+                {
+                    var comparator = _versionExpressionParser.CreateComparator(versionExpression);
+                    var versionText = _layoutRule.ProvideVersion(assetPath, assetType, isFolder, doSetup);
+
+                    if (string.IsNullOrEmpty(versionText) && _layoutRule.Settings.ExcludeUnversioned.Value)
+                        continue;
+
+                    // If the version is not satisfied, return false.
+                    if (!string.IsNullOrEmpty(versionText) && Version.TryCreate(versionText, out var version) &&
+                        !comparator.IsSatisfied(version))
+                        continue;
+                }
+
+                if (!groupedGuids.TryGetValue(addressableGroup.name, out var assetGuids))
+                {
+                    assetGuids = new List<string>();
+                    groupedGuids.Add(addressableGroup.name, assetGuids);
+                }
+                assetGuids.Add(assetGuid);
+            }
+
+            foreach (var kvp in groupedGuids)
+            {
+                var entryAdapters = _addressableSettingsAdapter.CreateOrMoveEntries(kvp.Key, kvp.Value);
+                foreach (var entryAdapter in entryAdapters)
+                {
+                    var assetPath = entryAdapter.Address;
+                    var assetType = _assetDatabaseAdapter.GetMainAssetTypeAtPath(assetPath);
+                    var isFolder = _assetDatabaseAdapter.IsValidFolder(assetPath);
+                
+                    UpdateAssetLabel(entryAdapter, assetPath, assetType, isFolder, doSetup);
+                }   
+            }
+        }
+
+        /// <summary>
+        ///     Apply the layout rule to the addressable settings.
+        /// </summary>
+        /// <param name="assetPath"></param>
+        /// <param name="doSetup">
+        ///     If true, setup rules before providing.
+        ///     When you call this method multiple times, set this false and call <see cref="Setup" /> before.
+        ///     If you call this method only once, set this true and don't call <see cref="Setup" />.
+        /// </param>
+        /// <param name="versionExpression"></param>
+        /// <returns>
+        ///     If the layout rule was applied to the addressable asset system, return true.
+        ///     Returns false if no suitable layout rule was found.
+        /// </returns>
+        public bool TryAddEntry(string assetPath, bool doSetup, string versionExpression = null)
+        {
+            var assetGuid = _assetDatabaseAdapter.AssetPathToGUID(assetPath);
             var assetType = _assetDatabaseAdapter.GetMainAssetTypeAtPath(assetPath);
             var isFolder = _assetDatabaseAdapter.IsValidFolder(assetPath);
 
@@ -114,12 +180,22 @@ namespace SmartAddresser.Editor.Core.Models.Services
             var entryAdapter = _addressableSettingsAdapter.CreateOrMoveEntry(addressableGroupName, assetGuid);
             entryAdapter.SetAddress(address);
 
+            UpdateAssetLabel(entryAdapter, assetPath, assetType, isFolder, doSetup);
+
+            return true;
+        }
+
+        internal void UpdateAssetLabel(IAddressableAssetEntryAdapter entryAdapter, string assetPath, System.Type assetType,
+            bool isFolder, bool doSetup)
+        {
             // Add labels to addressable settings if not exists.
             var labels = _layoutRule.ProvideLabels(assetPath, assetType, isFolder, doSetup);
             var addressableLabels = _addressableSettingsAdapter.GetLabels();
             foreach (var label in labels)
+            {
                 if (!addressableLabels.Contains(label))
                     _addressableSettingsAdapter.AddLabel(label);
+            }
 
             // Remove old labels.
             var oldLabels = entryAdapter.Labels.ToArray();
@@ -129,8 +205,6 @@ namespace SmartAddresser.Editor.Core.Models.Services
             // Add new labels.
             foreach (var label in labels)
                 entryAdapter.SetLabel(label, true);
-
-            return true;
         }
     }
 }
